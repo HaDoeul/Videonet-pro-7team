e4d30478f9661515d49ec977bf8767d0523d4b1a
HaDoeul <gmleh3991@gmail.com>
2025-12-03 11:34:32 +0900

    Revert "image comppress"
    This reverts commit 57987cb8ebb3546acf4176857cbad8954a06d0d2.

--- frontend/src/global.d.ts
+++
@@ -1,9 +0,0 @@
-// src/global.d.ts
-export {};
-
-declare global {
-  interface Window {
-    cv: any;                         // OpenCV.js가 여기에 올라감
-    onOpenCvReadyCallback?: () => void;  // React에서 설정
-  }
-}
